c9548a4a5a298f9882f52d9203616689417ab684
genghiskhan3 <mattleblanc333@gmail.com>
2014-10-07 13:01:35 -0400

    Create TestPentagonalNumber.java

---
+++ TestPentagonalNumber.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class TestPentagonalNumber{
+	public static void main(String[] args) {
+		StringBuilder string = new StringBuilder();
+
+		int count = 0;
+		for (int i = 1; i <= 100; i++){
+
+			int cur = getPentagonalNumber(i);
+
+			if (count < 9){
+				string.append(cur + " ");
+				count++;
+			} else{
+				count = 0;
+				string.append(cur + " \n");
+			}
+		}
+
+		System.out.print(string);
+	}
+
+	public static int getPentagonalNumber(int n){
+		return ((3 * n) - 1) / 2;
+	}
+}

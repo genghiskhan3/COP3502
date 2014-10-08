cb6678f6bb76d512aee8ffd3fab2b2952616b9fd
genghiskhan3 <mattleblanc333@gmail.com>
2014-10-08 11:25:32 -0400

    Update PrintTrigValues.java

--- PrintTrigValues.java
+++ PrintTrigValues.java
@@ -1,16 +1,10 @@
-import java.util.*;
-
 class PrintTrigValues{
 	public static void main(String[] args) {
-
 		System.out.printf("%-12s%-12s%-12s%n", "Degree", "Sin", "Cos");
-
+		
 		for (int i = 0; i <= 360; i += 10){
-			double cos = Math.cos(Math.toRadians(i));
-			double sin = Math.sin(Math.toRadians(i));
-
+			double cos = Math.cos(Math.toRadians(i)); double sin = Math.sin(Math.toRadians(i));
 			System.out.printf("%-12d%-12.4f%-12.4f%n", i, sin, cos);
-
 		}
 	}
 }

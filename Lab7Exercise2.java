d00ac4b6e55e2e5ccf065be69fb6f63ba06d129a
genghiskhan3 <mattleblanc333@gmail.com>
2014-10-13 11:08:12 -0400

    Update Lab7Exercise2.java

--- Lab7Exercise2.java
+++ Lab7Exercise2.java
@@ -12,13 +12,40 @@ class Lab7Exercise2{
 			values[i] = (double)Integer.parseInt(array[i]);
 		}
 
-		System.out.println(getArea(values[0], values[1], values[2]));
+		double area = getArea(values[0], values[1], values[2]);
+		if (area == -1.0){
+			displayError();
+		} else{
+			System.out.println(area);
+		}
 	}
 
 	public static double getArea(double a, double b, double c){
-		double s = (a + b + c) / 2;
+		
+		if (validTriangle(a,b,c)){
+			double s = (a + b + c) / 2;
 
-		double parameter = s * ((s - a)*(s - b)*(s - c));
-		return Math.sqrt(parameter);
+			double parameter = s * ((s - a)*(s - b)*(s - c));
+			return Math.sqrt(parameter);
+		} else{
+			return-1.0;
+		}
+	}
+
+	public static boolean validTriangle(double a, double b, double c){
+		double max = 0.0;
+		max = Math.max(a, max);
+		max = Math.max(b, max);
+		max = Math.max(c, max);
+
+		if (max >= ((a + b + c) - max)){
+			return false;
+		} else{
+			return true;
+		}
+	}
+
+	public static void displayError(){
+		System.out.println("Cannot form a triangle");
 	}
 }
